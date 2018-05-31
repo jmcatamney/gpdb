cc697453e5ffcb2c15def587f218406722cbce50
Jamie McAtamney <jmcatamney@pivotal.io>
2018-05-31 11:46:40 -0700

    WIP switch to Cobra

--- gpMgmt/go-utils/src/gpscp/gpscp.go
+++ gpMgmt/go-utils/src/gpscp/gpscp.go
@@ -16,6 +16,8 @@ import (
 	"github.com/greenplum-db/gp-common-go-libs/gplog"
 	"github.com/greenplum-db/gp-common-go-libs/iohelper"
 	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 var (
@@ -36,52 +38,75 @@ var (
 var (
 	debug          *bool
 	hostFile       *string
-	hostNames      ArrayFlags
+	hostNames      *[]string
 	quiet          *bool
 	verbose        *bool
 	perSegmentCopy *bool
+	version        string
 	copyPath       string
 	fileToCopy     string
 )
 
-func initializeFlags() {
-	perSegmentCopy = flag.Bool("per-segment-copy", false, "Copy file to each segment.")
-	debug = flag.Bool("debug", false, "Print verbose and debug log messages")
-	flag.Var(&hostNames, "hostname", "The name of a single host that will participate in the SCP session.  --hostname can be specified multiple times.")
-	hostFile = flag.String("hostfile", "", "The absolute path of a file containing a list of hosts to participate in the SCP session")
-	quiet = flag.Bool("quiet", false, "Suppress non-warning, non-error log messages")
-	verbose = flag.Bool("verbose", false, "Print verbose log messages")
+func initializeFlags(cmd *cobra.Command) {
+	debug = cmd.Flags().Bool("debug", false, "Print verbose and debug log messages")
+	hostFile = cmd.Flags().String("hostfile", "", "The absolute path of a file containing a list of hosts to participate in the SCP session")
+	hostNames = cmd.Flags().StringSlice("hostname", []string{}, "The name of a single host that will participate in the SCP session.  --hostname can be specified multiple times.")
+	quiet = cmd.Flags().Bool("quiet", false, "Suppress non-warning, non-error log messages")
+	verbose = cmd.Flags().Bool("verbose", false, "Print verbose log messages")
+	cmd.Flags().Bool("version", false, "Print version number and exit")
 }
 
-func initializeCommandLineArgs() {
-	args := flag.Args()
+func initializeCommandLineArgs(cmd *cobra.Command, args []string) error {
 	if len(args) != 2 {
 		fmt.Println("Usage: gpscp [local file] [remote copy path]")
 		os.Exit(1)
 	}
 	fileToCopy = args[0]
 	copyPath = args[1]
+	return nil
 }
 
 func main() {
-	defer DoTeardown()
-	DoInit()
-	DoFlagValidation()
-	DoSetup()
-	DoScp()
+	var rootCmd = &cobra.Command{
+		Use:     "gpscp",
+		Short:   "gpscp is a utility for SCPing files to and from multiple hosts in parallel",
+		Args:    initializeCommandLineArgs,
+		Version: GetVersion(),
+		Run: func(cmd *cobra.Command, args []string) {
+			defer DoTeardown()
+			DoFlagValidation(cmd)
+			DoSetup()
+			DoScp()
+		}}
+	DoInit(rootCmd)
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(2)
+	}
 }
 
-func DoInit() {
+func DoInit(cmd *cobra.Command) {
 	CleanupGroup = &sync.WaitGroup{}
 	CleanupGroup.Add(1)
 	gplog.InitializeLogging("gpscp", "")
-	initializeFlags()
+	initializeFlags(cmd)
 	InitializeSignalHandler(DoCleanup, "gpscp process", &wasTerminated)
 }
 
-func DoFlagValidation() {
+func DoFlagValidation(cmd *cobra.Command) {
 	flag.Parse()
-	CheckExclusiveFlags("hostFile", "hostNames")
+	CheckExclusiveFlags(cmd.Flags(), "hostFile", "hostNames")
+}
+
+func CheckExclusiveFlags(flags *pflag.FlagSet, flagNames ...string) {
+	numSet := 0
+	for _, name := range flagNames {
+		if flags.Changed(name) {
+			numSet++
+		}
+	}
+	if numSet > 1 {
+		gplog.Fatal(errors.Errorf("The following flags may not be specified together: %s", strings.Join(flagNames, ", ")), "")
+	}
 }
 
 func SetLoggerVerbosity() {
@@ -97,20 +122,19 @@ func SetLoggerVerbosity() {
 // This function handles setup that must be done after parsing flags.
 func DoSetup() {
 	SetLoggerVerbosity()
-	initializeCommandLineArgs()
 
 	globalCluster = cluster.NewCluster(nil)
 	if *hostFile != "" {
-		hostNames = iohelper.MustReadLinesFromFile(*hostFile)
+		*hostNames = iohelper.MustReadLinesFromFile(*hostFile)
 	}
-	if len(hostNames) == 0 {
+	if len(*hostNames) == 0 {
 		gplog.Fatal(errors.Errorf("No hostnames specified.  Use the --hostfile or --hostname flag to specify hostnames."), "")
 	}
 }
 
 func DoScp() {
-	hostNames = UniquifyHostnames(hostNames)
-	commands := BuildCommands(hostNames)
+	*hostNames = UniquifyHostnames(*hostNames)
+	commands := BuildCommands(*hostNames)
 	remoteOutput := globalCluster.ExecuteClusterCommand(cluster.ON_HOSTS, commands)
 	globalCluster.CheckClusterError(remoteOutput, "Unable to execute copy", func(contentID int) string {
 		return "Error executing copy"
@@ -131,7 +155,6 @@ func UniquifyHostnames(hostnames []string) []string {
 }
 
 func BuildCommands(hostnames []string) map[int][]string {
-	fmt.Println("CopyPath:", copyPath)
 	if !strings.Contains(copyPath, "=") {
 		gplog.Fatal(errors.Errorf("No substitution character present in copy path."), "")
 	}
@@ -190,4 +213,8 @@ func DoCleanup() {
 		CleanupGroup.Done()
 	}()
 	gplog.Verbose("Beginning cleanup")
+}
+
+func GetVersion() string {
+	return version
 }
